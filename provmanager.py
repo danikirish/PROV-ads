b90a4a1d901a229d62c68b9f8611fa1ce7e4449b
Daniyar Irishev <irishevdaniyar@gmail.com>
2020-03-27 18:06:28 +0000

    start buttons

--- provmanager.py
+++ provmanager.py
@@ -21,40 +21,45 @@ from prov.dot import prov_to_dot
 
 
 class ProvManager():
-    def __init__(self, output_fp=path.join(os.getcwd(), "results/"), db_fp="/Users/Danik/Desktop/KCL/YEAR_3/PRJ/Crawls/crawl-data.sqlite"):
+    def __init__(self, output_fp=path.join(os.getcwd(), "Crawls", "results"), db_fp="/Users/Danik/Desktop/KCL/YEAR_3/PRJ/Crawls/crawl-data.sqlite"):
         self.db_fp = db_fp
         self.output_fp = output_fp
         self.documents = {}
-        self.all_tp_hosts = {1: [],
-                           2: [],
-                           3: [],
-                           4: []}
+        self.all_tp_hosts = {}
         self.cur = None
-        if not path.exists(self.output_fp):
-            os.mkdir(self.output_fp)
-            
+        self.last_crawl = None
         
     def main(self):
         self.db_connect()
         self.create_prov()
         self.record_prov()
         self.write_prov()
-        self.all_cookie_stats()
+        self.all_cookie_stats(self.last_crawl)
     
     def db_connect(self):
         connection = lite.connect(self.db_fp)
         self.cur = connection.cursor()
-    
+        self.cur.execute("select crawl_id from crawl")
+        self.last_crawl = max(self.cur.fetchall())[0] #TODO: Refactor the db calls and variables (eg last_crawl, visit_id)
+        self.output_fp = self.output_fp + str(self.last_crawl)
+        date_time = self.cur.execute("select start_time from crawl where crawl_id=?", [str(self.last_crawl)]).fetchone()[0]
+        date, time = date_time.split(" ")[0], date_time.split(" ")[1][:-3:]
+        print("Reading from: ", self.db_fp)
+        print("Analysing crawl %d on %s at %s" % (self.last_crawl, date, time))
+        # print(self.output_fp)
+            
+
+
     def create_prov(self):
-        for visit_id in self.cur.execute("select visit_id from site_visits"):
+        for visit_id in self.cur.execute("select visit_id from site_visits where crawl_id=?", [str(self.last_crawl)]):
             document = ProvDocument()
             document.set_default_namespace('http://danik.com')
             self.documents[visit_id[0]] = document
 #         return document
     
-    def retrieve_tp_hosts(self,visit_id, site_url):
+    def retrieve_tp_hosts(self, visit_id, site_url):
         hosts = []
-        for h in self.cur.execute("select host from javascript_cookies where visit_id=?", str(visit_id)):
+        for h in self.cur.execute("select host from javascript_cookies where visit_id=?", [str(visit_id)]):
             split = h[0].split('.')
             if 'www' in split: split.remove('www')
             host = split[1]
@@ -71,7 +76,7 @@ class ProvManager():
             doc.agent('user')
             doc.activity('performCrawl')
             doc.entity('visit%d'%visit)
-            self.cur.execute("select site_url from site_visits where visit_id=?",str(visit))
+            self.cur.execute("select site_url from site_visits where visit_id=?", [str(visit)])
             site_url = self.cur.fetchone()[0][7:]
             doc.entity(site_url)
             doc.entity('syncing_algorithm')
@@ -116,38 +121,44 @@ class ProvManager():
                 e = doc.entity(tp)
                 doc.hadMember(tp_cookies, e)
             self.all_tp_hosts[visit] = tp_hosts
-        print("finished record:")
-        print(self.all_tp_hosts[2])
+        # print("finished record:")
+        # print(self.all_tp_hosts[2])
             
     def write_prov(self):
+        if not path.exists(self.output_fp):
+            os.mkdir(self.output_fp)
+        print("Writing to: ", self.output_fp, "\n")
         for visit, document in self.documents.items():
             dot = prov_to_dot(document)
             dot.write_png(path.join(self.output_fp,'visit%d.png' %visit))
 #             file = open(path.join(self.output_fp,'visit%d.png' %visit), 'w+')
 #             file.write(dot)
 #             file.close()
-            print("writing")
+            print("writing visit%d.png"%visit)
 #             dot.write_png('visit%d.png' %visit)
 
     def cookie_stats(self, visit_id, site_url = ""):
         tp_cookies = self.all_tp_hosts[visit_id]
-        self.cur.execute("select count(id) from javascript_cookies where visit_id=?", str(visit_id))
+        self.cur.execute("select count(id) from javascript_cookies where visit_id=?", [str(visit_id)])
         cookies_num = self.cur.fetchone()[0]
-        print("This visit resulted in %d cookies being recorded on your browser." % cookies_num)
+        print("\nVisit to %s (id: %d) resulted in %d cookies being recorded on your browser." % (site_url, visit_id, cookies_num))
         if len(tp_cookies) == 0:
             print("None of which are 3rd party")
         else:
             print("Of which %d are 3rd party."%len(tp_cookies))
             print("List of 3rd party trackers: ")
-            print(set(tp_cookies))
+            print(set(tp_cookies), "\n")
             self.cookie_pie(cookies_num, len(tp_cookies), site_url)
             
-    def all_cookie_stats(self,):
+    def all_cookie_stats(self, crawl_id):
+        # print(self.all_tp_hosts)
         for vid, cs in self.all_tp_hosts.items():
-            self.cur.execute("select site_url from site_visits where visit_id=?",str(vid)) #TODO: Refactor this
+            # print("VISIT %d" %vid)
+            self.cur.execute("select site_url from site_visits where visit_id=?", [str(vid)]) #TODO: Refactor this
             site_url = self.cur.fetchone()[0][7:]
-            print("\nVisit %d to %s" % (vid, site_url), end='')
-            self.cookie_stats(vid, site_url = site_url)
+            # print("GOT %s"%site_url)
+            # print("\nVisit %d to %s" % (vid, site_url), end='')
+            self.cookie_stats(vid, site_url=site_url)
         input("Press enter to finish...")
             
     def cookie_pie(self, num_total, num_tp, site_url=""):
