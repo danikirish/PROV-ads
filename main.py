82ec296ac6093e58592fe61db3c0c36e59741fb2
Daniyar Irishev <irishevdaniyar@gmail.com>
2020-04-09 16:49:25 +0100

    script work

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from provmanager import ProvManager
 # import os
-import subprocess
-import sys
-subprocess.run("pwd")
+# from subprocess import Popen, PIPE
+# import sys
+
+# venv = "/Users/Danik/Desktop/KCL/YEAR_3/PRJ/OpenWPM/venv/bin/activate"
+# with open(venv) as f:
+#     code = compile(f.read(), venv, 'exec')
+#     exec(code, dict(__file__=venv))
+# subprocess.Popen(["sudo", ".", "/Users/Danik/Desktop/KCL/YEAR_3/PRJ/OpenWPM/venv/bin/activate"])
+# process = Popen(['source', '/Users/Danik/Desktop/KCL/YEAR_3/PRJ/OpenWPM/venv/bin/activate'], stdout=PIPE, stderr=PIPE)
+# Popen(['sudo', '.', './OpenWPM/venv/bin/activate'])
+# stdout, stderr = process.communicate()
 # subprocess.call(["source", "/Users/Danik/Desktop/KCL/YEAR_3/PRJ/OpenWPM/venv/bin/activate"])
 # subprocess.call(["source", "/OpenWPM/venv/bin/activate"])
 # subprocess.run(["cd", "/Users/Danik/Desktop/KCL/YEAR_3/PRJ/Notebooks"])
-subprocess.run("source", cwd="OpenWPM/venv/bin/")
+# subprocess.run("source", cwd="OpenWPM/venv/bin/")
 # sys.path.append("/Users/Danik/Desktop/KCL/YEAR_3/PRJ/OpenWPM/")
 # print("STARTING CRAWL...")
 # import prj
@@ -14,6 +22,6 @@ subprocess.run("source", cwd="OpenWPM/venv/bin/")
 
 
 
-# print("STARTING ANALYSIS...")
-# provman = ProvManager()
-# provman.main()
+print("STARTING ANALYSIS...")
+provman = ProvManager()
+provman.main()
--- provmanager.py
+++ provmanager.py
@@ -12,6 +12,7 @@ from os import path
 import seaborn as sns
 import matplotlib.pyplot as plt
 from prov.dot import prov_to_dot
+import matplotlib.image as mpimg
 # from IPython.display import Image
 # import python_main_function 
 
@@ -28,13 +29,19 @@ class ProvManager():
         self.all_tp_hosts = {}
         self.cur = None
         self.last_crawl = None
+        self.tester = None
         
     def main(self):
         self.db_connect()
         self.create_prov()
         self.record_prov()
         self.write_prov()
-        self.all_cookie_stats(self.last_crawl)
+        # img = mpimg.imread(self.tester)
+        # impgplot = plt.imshow(img)
+        # plt.show()
+        # input('...')
+
+        # self.all_cookie_stats(self.last_crawl)
     
     def db_connect(self):
         connection = lite.connect(self.db_fp)
@@ -117,9 +124,12 @@ class ProvManager():
         #         hosts.append(host)
         #         if host in trackers:
         #             hosts.append(host)
+            added_hosts = set()
             for tp in tp_hosts:
-                e = doc.entity(tp)
-                doc.hadMember(tp_cookies, e)
+                if not tp in added_hosts:
+                    e = doc.entity(tp)
+                    doc.hadMember(tp_cookies, e)
+                    added_hosts.add(tp)
             self.all_tp_hosts[visit] = tp_hosts
         # print("finished record:")
         # print(self.all_tp_hosts[2])
@@ -131,54 +141,85 @@ class ProvManager():
         for visit, document in self.documents.items():
             dot = prov_to_dot(document)
             dot.write_png(path.join(self.output_fp,'visit%d.png' %visit))
+            self.tester = path.join(self.output_fp,'visit%d.png' %visit)
 #             file = open(path.join(self.output_fp,'visit%d.png' %visit), 'w+')
 #             file.write(dot)
 #             file.close()
             print("writing visit%d.png"%visit)
 #             dot.write_png('visit%d.png' %visit)
-
-    def cookie_stats(self, visit_id, site_url = ""):
-        tp_cookies = self.all_tp_hosts[visit_id]
-        self.cur.execute("select count(id) from javascript_cookies where visit_id=?", [str(visit_id)])
-        cookies_num = self.cur.fetchone()[0]
-        print("\nVisit to %s (id: %d) resulted in %d cookies being recorded on your browser." % (site_url, visit_id, cookies_num))
-        if len(tp_cookies) == 0:
-            print("None of which are 3rd party")
-        else:
-            print("Of which %d are 3rd party."%len(tp_cookies))
-            print("List of 3rd party trackers: ")
-            print(set(tp_cookies), "\n")
-            self.cookie_pie(cookies_num, len(tp_cookies), site_url)
+#
+#     def cookie_stats(self, visit_id, site_url=""):
+#         tp_cookies = self.all_tp_hosts[visit_id]
+#         self.cur.execute("select count(id) from javascript_cookies where visit_id=?", [str(visit_id)])
+#         cookies_num = self.cur.fetchone()[0]
+#         print("\nVisit to %s (id: %d) resulted in %d cookies being recorded on your browser." % (site_url, visit_id, cookies_num))
+#         if len(tp_cookies) == 0:
+#             print("None of which are 3rd party")
+#         else:
+#             print("Of which %d are 3rd party."%len(tp_cookies))
+#             print("List of 3rd party trackers: ")
+#             print(set(tp_cookies), "\n")
+#             # self.cookie_pie(cookies_num, len(tp_cookies), site_url)
+#         return cookies_num, len(tp_cookies)
             
-    def all_cookie_stats(self, crawl_id):
-        # print(self.all_tp_hosts)
-        for vid, cs in self.all_tp_hosts.items():
-            # print("VISIT %d" %vid)
-            self.cur.execute("select site_url from site_visits where visit_id=?", [str(vid)]) #TODO: Refactor this
-            site_url = self.cur.fetchone()[0][7:]
-            # print("GOT %s"%site_url)
-            # print("\nVisit %d to %s" % (vid, site_url), end='')
-            self.cookie_stats(vid, site_url=site_url)
-        input("Press enter to finish...")
-            
-    def cookie_pie(self, num_total, num_tp, site_url=""):
-        color_palette_list = ['#009ACD', '#ADD8E6', '#63D1F4', '#0EBFE9', 
-                              '#C1F0F6', '#0099CC']
-        labels = ['1st Party', '3rd Party']
-        plt.rcParams['font.sans-serif'] = 'Arial'
-        plt.rcParams['font.family'] = 'sans-serif'
-        plt.rcParams['text.color'] = '#e8001b'
-        plt.rcParams['axes.labelcolor']= '#e8001b'
-        plt.rcParams['xtick.color'] = '#e8001b'
-        plt.rcParams['ytick.color'] = '#e8001b'
-        plt.rcParams['font.size']=12
-        values = [num_total -num_tp, num_tp]
-        explode = (0.1, 0)
+    # def all_cookie_stats(self, crawl_id):
+    #     cookie_nums = []
+    #     tp_cookies_len = []
+    #     site_urls = []
+    #     # print(self.all_tp_hosts)
+    #     for vid, cs in self.all_tp_hosts.items():
+    #         # print("VISIT %d" %vid)
+    #         self.cur.execute("select site_url from site_visits where visit_id=?", [str(vid)]) #TODO: Refactor this
+    #         site_url = self.cur.fetchone()[0][7:]
+    #         # print("GOT %s"%site_url)
+    #         # print("\nVisit %d to %s" % (vid, site_url), end='')
+    #         cnum, tplen = self.cookie_stats(vid, site_url=site_url)
+    #         cookie_nums.append(cnum)
+    #         tp_cookies_len.append(tplen)
+    #         site_urls.append(site_url)
+    #     self.cookie_pie(cookie_nums, tplen, site_urls)
+    #
+    #     input("Press enter to finish...")
 
-        fig, ax = plt.subplots()
-        ax.pie(values, labels=labels, explode=explode, colors=color_palette_list[0:2], autopct='%1.0f%%', shadow=False, startangle=90, pctdistance=0.8, labeldistance=1.1)
-        ax.axis('equal')
-        ax.set_title('Proportion of 3rd party cookies from %s\nTotal number of cookies: %d' %(site_url, num_total))
-        ax.legend(frameon=False, bbox_to_anchor=(1.5, 0.8))
-        fig.show()
 
+    # def cookie_pie(self, num_total, num_tp, site_urls):
+    #     for ind in range(len(site_urls)):
+    #
+    #         color_palette_list = ['#009ACD', '#ADD8E6', '#63D1F4', '#0EBFE9',
+    #                               '#C1F0F6', '#0099CC']
+    #         labels = ['1st Party', '3rd Party']
+    #         plt.rcParams['font.sans-serif'] = 'Arial'
+    #         plt.rcParams['font.family'] = 'sans-serif'
+    #         plt.rcParams['text.color'] = '#e8001b'
+    #         plt.rcParams['axes.labelcolor']= '#e8001b'
+    #         plt.rcParams['xtick.color'] = '#e8001b'
+    #         plt.rcParams['ytick.color'] = '#e8001b'
+    #         plt.rcParams['font.size']=12
+    #         values = [len(num_total) - num_tp, num_tp]
+    #         explode = (0.1, 0)
+    #
+    #     fig, ax = plt.subplots()
+    #     ax.pie(values, labels=labels, explode=explode, colors=color_palette_list[0:2], autopct='%1.0f%%', shadow=False, startangle=90, pctdistance=0.8, labeldistance=1.1)
+    #     ax.axis('equal')
+    #     ax.set_title('Proportion of 3rd party cookies from %s\nTotal number of cookies: %d' %(site_urls[ind], len(num_total)))
+    #     ax.legend(frameon=False, bbox_to_anchor=(1.5, 0.8))
+    #     fig.show()
+
+
+#
+# class Index:
+#     ind = 0
+#
+#     def next(self, event):
+#         self.ind += 1
+#         i = self.ind % len(freqs)
+#         ydata = np.sin(2*np.pi*freqs[i]*t)
+#         l.set_ydata(ydata)
+#         plt.draw()
+#
+#     def prev(self, event):
+#         self.ind -= 1
+#         i = self.ind % len(freqs)
+#         ydata = np.sin(2*np.pi*freqs[i]*t)
+#         l.set_ydata(ydata)
+#         plt.draw()
